63a8d253a499595aa57111df578ce2fb280d4eca
kojotak <tomas.beneda@gmail.com>
2017-08-19 19:21:47 +0200

    section 18

---
+++ src/components/search_bar.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+//function is stateless and usable here...
+//const SearchBar = () => {
+//  return <input />;
+//}
+
+class SearchBar extends Component {
+
+  render(){
+    return <input/>;
+  }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
 import React from 'react';
+
+//for registering our app into DOM
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+//console.developers.google.com -> YouTube Data API v3
+const YOUTUBE_API_KEY='AIzaSyAwlImkLsu8MNDStYF_VLD6Avzt1VV1dZ8';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+//defines Component which renders HTML
+const App =  () => { //'fat arrow'same as: function()
+  return (
+    <div>
+      <SearchBar/>
+    </div>
+  )
+}
+
+//but this component have to be registered in the DOM
+
+//naive and wrong attempt...
+//React.render(App);
+//this will cause an:
+// Error: ReactDOM.render(): Invalid component element. Instead of passing a component class,
+// make sure to instantiate it by passing it to React.createElement.
+
+//still wrong:
+//ReactDOM.render(<App/>);
+// Error: _registerComponent(...):
+// Target container is not a DOM element.  bundle.js:1207:16
+
+ReactDOM.render(<App/>, document.querySelector('.container'));
+//fixed by specifying target container
