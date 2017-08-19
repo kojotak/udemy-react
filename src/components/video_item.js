d75a0be1e40b7e8af66f117c8286f994876c8f92
kojotak <tomas.beneda@gmail.com>
2017-08-19 20:32:48 +0200

    fixed displaying list of videos

--- src/components/video_item.js
+++ src/components/video_item.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
-const VideoItem = (props) => {
-  return <li>video</li>;
+const VideoItem = ({video}) => {
+  //ES6: argument {video} is same as reading it from
+  //const video = props.video;
+
+  const imageUrl = video.snippet.thumbnails.default.url;
+
+  return (
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src={imageUrl} />
+        </div>
+        <div className="media-body">
+          <div className="media-heading">{video.snippet.title}</div>
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default VideoItem;
