cc886e3464e18e7e65fa29d33b1c45f4cad3d243
kojotak <tomas.beneda@gmail.com>
2017-08-19 20:21:34 +0200

    lesson 25 video list

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import VideoItem from './video_item';
 
+//function is enough here, we are only displaying things
 const VideoList = (props) => {
-  //function is enough here, we are only displaying things
+
+  //is actually an array...
+  const videoItems = props.videos.map( (video) => {
+    return <VideoItem video={video} />;
+  });
 
   return (
     <ul className="col-md-4 list-group">
-      {props.videos.length}
+      {videoItems}
     </ul>
   )
 };
