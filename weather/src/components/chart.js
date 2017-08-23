5d21e7dbbcbfb72a14b0bccc16fac1ca5aeac93b
kojotak <tomas.beneda@gmail.com>
2017-08-23 21:57:12 +0200

    added average line

--- weather/src/components/chart.js
+++ weather/src/components/chart.js
@@ -1,13 +1,14 @@
 //simple component displaying some data... do not need class nor redux
 
 import React from 'react';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
 
 export default (props) => {
   return (
     <div>
       <Sparklines height={120} width={120} data={props.data}>
         <SparklinesLine color={props.color}/>
+        <SparklinesReferenceLine type="avg" />
       </Sparklines>
     </div>
   );
