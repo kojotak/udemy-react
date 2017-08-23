24629c98af9ac5a921bd1ef8519ba2d7cc7be32c
kojotak <tomas.beneda@gmail.com>
2017-08-23 21:11:28 +0200

    fixed weather URL and added console logging

--- weather/src/actions/index.js
+++ weather/src/actions/index.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const API_KEY = 'f32bb496dede4e1a31ca375058b182b1';
-const ROOT_URL = 'http://samples.openweathermap.org/data/2.5/forecast?appid=' + API_KEY;
+const ROOT_URL = 'http://api.openweathermap.org/data/2.5/forecast?appid=' + API_KEY;
 
 //recommended way how to re-use action names
 export const FETCH_WEATHER = 'FETCH_WEATHER';
@@ -11,6 +11,8 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 export function fetchWeather(city){
   //ES6 syntactic sugar for formating strings
   const url = `${ROOT_URL}&q=${city},cs`;
+
+  console.log('Request: ', request);
 
   //promise
   const request = axios.get(url);
