9db05e23ecfdf37d6e190ed6f6ac1b749f69dd8d
kojotak <tomas.beneda@gmail.com>
2017-08-31 22:22:32 +0200

    prepared form skeleton using redux-form

--- blog/src/components/post_new.js
+++ blog/src/components/post_new.js
@@ -1,8 +1,31 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { Field, reduxForm } from 'redux-form';
 
 class PostNew extends Component {
+
+  //we need to pass some reference to field in order to connect event handlers
+  //or to access passsed additional parameters such as label
+  renderField(field){
+    return (
+      <div className="form-group">
+        <label>{field.label}</label>
+        {/*
+            instead of wiring all those event handlers like:
+              onChange={field.input.onChange}
+              onFocus={field.input.onFocus}
+              onBlur={field.input.onBlur}
+            we'll use a ES6 shortcut, which says "use all from": ...field.input
+        */}
+        <input
+          type="text"
+          className="form-control"
+          {...field.input}
+        />
+      </div>
+    );
+  }
 
   render(){
     return (
@@ -11,11 +34,36 @@ class PostNew extends Component {
           <Link className="btn btn-primary" to="/">Home</Link>
         </div>
 
-
-        Create new post
+        <h3>Create new post</h3>
+        <form>
+          {/*
+              the Field itself is just "a thing" which communicates with redux
+              component in field is function for displaying input field
+              label is our optional attribute passed to inner component
+          */}
+          <Field
+            name="title"
+            label="Title"
+            component={this.renderField}>
+          </Field>
+          <Field
+            name="tags"
+            label="Tags"
+            component={this.renderField}>
+          </Field>
+          <Field
+            name="content"
+            label="Post content"
+            component={this.renderField}>
+          </Field>
+        </form>
       </div>
     );
   }
 }
 
-export default PostNew;
+//we pass single argument - function - for form configuration:
+//form: unique identifier
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostNew);
