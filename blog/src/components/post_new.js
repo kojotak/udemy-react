42a4ec9dbc3c56fc418cad6bbfb30e3b43d3124a
kojotak <tomas.beneda@gmail.com>
2017-08-31 21:46:03 +0200

    skeleton for "new post" form

---
+++ blog/src/components/post_new.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class PostNew extends Component {
+
+  render(){
+    return (
+      <div>
+        Hello
+      </div>
+    );
+  }
+}
+
+export default PostNew;
--- blog/src/index.js
+++ blog/src/index.js
@@ -5,10 +5,11 @@ import { createStore, applyMiddleware } from 'redux';
 
 //BroserRouter - works with browser history
 //Route - react component provides configuration for routes (=where to go)
-import { BrowserRouter, Route  } from 'react-router-dom';
+import { BrowserRouter, Route, Switch  } from 'react-router-dom';
 
 import reducers from './reducers';
 import PostIndex from './components/post_index';
+import PostNew from './components/post_new';
 import promise from 'redux-promise';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
@@ -17,7 +18,15 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/" component={PostIndex} />
+        {/*
+            Router matches all components...
+            Switch choosed just one from them...
+            (without a switch, the /posts/new will display both components)
+        */}
+        <Switch>
+          <Route path="/posts/new" component={PostNew} />
+          <Route path="/"  component={PostIndex} />
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>
